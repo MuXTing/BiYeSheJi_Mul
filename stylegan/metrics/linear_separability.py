bea26ebd900fbc74c53eafd2a3698392fed2d134
孙光林 <44129861+MuXTing@users.noreply.github.com>
2021-04-23 09:41:31 +0800

    Update linear_separability.py

--- stylegan/metrics/linear_separability.py
+++ stylegan/metrics/linear_separability.py
@@ -147,10 +147,10 @@ class LS(metric_base.MetricBase):
         results = {key: np.concatenate([value[key] for value in results], axis=0) for key in results[0].keys()}
 
         np.save('results/stylegan-dlatents.npy', results['dlatents'])
-        np.save('results/34_score.npy', results[34][:, 0].reshape((-1,1)))
-        np.save('results/8_score.npy', results[3][:, 0].reshape((-1,1)))
+#         np.save('results/34_score.npy', results[34][:, 0].reshape((-1,1)))
+#         np.save('results/8_score.npy', results[3][:, 0].reshape((-1,1)))
         np.save('results/9_score.npy', results[8][:, 0].reshape((-1,1)))
-        np.save('results/3_score.npy', results[9][:, 0].reshape((-1,1)))
+#         np.save('results/3_score.npy', results[9][:, 0].reshape((-1,1)))
 
 #       # Calculate conditional entropy for each attribute.
 #         conditional_entropies = defaultdict(list)
